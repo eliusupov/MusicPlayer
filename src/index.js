369384629468c11a252ebd830c312e5942f32f65
Eli <eliusupov123@gmail.com>
2017-12-24 18:09:38 +0200

    added .htaccsess file to redirect root to dist folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style/font-awesome/fontawesome-all.min';
+import './style/font-awesome/fontawesome-all.min.js';
 import './style/style.css';
 import './index.html';
 
